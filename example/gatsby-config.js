44b5b7c171638f41a2eb7c4fe646198cae69f2b9
mathieudutour <mathieu@dutour.me>
2020-05-14 18:17:21 +0200

    fix typo

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 });
 
 module.exports = {
-  pathPrefix: `/gatsby-source-roamresearch`
+  pathPrefix: `/gatsby-source-roamresearch`,
   siteMetadata: {
     title: `Gatsby with Roam Research`,
   },
