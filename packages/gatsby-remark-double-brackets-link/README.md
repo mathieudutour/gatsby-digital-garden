7ef52bb26bc225cd9ef9f71f625eabc6031a4706
mathieudutour <mathieu@dutour.me>
2020-05-15 17:14:02 +0200

    add gatsby-remark-double-brackets-link

---
+++ packages/gatsby-remark-double-brackets-link/README.md
@@ -0,0 +1,34 @@
+# gatsby-remark-double-brackets-link
+
+Transform `[[Link to page]]` into `[Link to page](titleToURL('Link to page'))`.
+
+## Installation
+
+```bash
+npm install gatsby-remark-double-brackets-link
+```
+
+## Usage
+
+Add the plugin to your Gatsby config:
+
+```js
+{
+  resolve: `gatsby-plugin-mdx`,
+  options: {
+    gatsbyRemarkPlugins: [
+      {
+        resolve: `gatsby-remark-double-brackets-link`,
+      },
+    ],
+  },
+}
+```
+
+By default, the plugin will resolve the url with:
+
+```js
+(title: string) => `/${slugify(title)}`;
+```
+
+You can override this behavior by passing a `titleToURL` option.
---
+++ packages/gatsby-remark-double-brackets-link/src/index.ts
@@ -0,0 +1,48 @@
+import visit from "unist-util-visit";
+import { Node } from "unist";
+import slugify from "slugify";
+
+const addDoubleBracketsLinks = (
+  { markdownAST }: { markdownAST: Node },
+  options?: { titleToURL: (title: string) => string }
+) => {
+  const titleToURL =
+    options?.titleToURL || ((title: string) => `/${slugify(title)}`);
+
+  visit(markdownAST, `linkReference`, (node, index, parent) => {
+    if (node.referenceType !== "shortcut") {
+      return;
+    }
+    const siblings = parent.children;
+    if (!siblings || !Array.isArray(siblings)) {
+      return;
+    }
+    const previous = siblings[index - 1];
+    const next = siblings[index + 1];
+
+    if (!previous || !next) {
+      return;
+    }
+
+    if (
+      previous.type !== "text" ||
+      previous.value[previous.value.length - 1] !== "[" ||
+      next.type !== "text" ||
+      next.value[0] !== "]"
+    ) {
+      return;
+    }
+
+    previous.value = previous.value.replace(/\[$/, "");
+    next.value = next.value.replace(/^\]/, "");
+
+    node.type = "link";
+    node.url = titleToURL(node.label as string);
+    node.title = node.label;
+    delete node.label;
+    delete node.referenceType;
+    delete node.identifier;
+  });
+};
+
+export default addDoubleBracketsLinks;
