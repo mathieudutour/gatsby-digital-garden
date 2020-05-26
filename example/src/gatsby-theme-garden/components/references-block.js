418708bdad53a8b8724b0ce8cebe9df247c52b05
mathieudutour <mathieu@dutour.me>
2020-05-26 18:55:40 +0200

    shadow the footer

---
+++ example/src/gatsby-theme-garden/components/references-block.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Reference from "gatsby-theme-garden/src/components/reference";
+
+import "gatsby-theme-garden/src/components/references-block.css";
+
+const ReferencesBlock = ({ references }) => {
+  const footer = (
+    <React.Fragment>
+      <p>
+        If you think this note resonated, be it positive or negative,{" "}
+        <a href="https://twitter.com/mathieudutour">ping me</a> on Twitter.
+      </p>
+    </React.Fragment>
+  );
+
+  if (!references.length) {
+    return <div className="references-block">{footer}</div>;
+  }
+
+  return (
+    <div className="references-block">
+      <h3>Referred in</h3>
+      <div>
+        {references.map((ref) => (
+          <Reference node={ref} key={ref.parent.id} />
+        ))}
+      </div>
+      <hr />
+      {footer}
+    </div>
+  );
+};
+
+export default ReferencesBlock;
--- packages/gatsby-theme-garden/src/components/references-block.js
+++ packages/gatsby-theme-garden/src/components/references-block.js
@@ -4,25 +4,8 @@ import Reference from "./reference";
 import "./references-block.css";
 
 const ReferencesBlock = ({ references }) => {
-  const footer = (
-    <React.Fragment>
-      <p>
-        If you think this note resonated, be it positive or negative,{" "}
-        <a href="https://twitter.com/mathieudutour">ping me</a> on Twitter.
-      </p>
-      <p>
-        The source for this website is on
-        {` `}
-        <a href="https://github.com/mathieudutour/gatsby-n-roamresearch/tree/master/example">
-          GitHub
-        </a>
-        .
-      </p>
-    </React.Fragment>
-  );
-
   if (!references.length) {
-    return <div className="references-block">{footer}</div>;
+    return null;
   }
 
   return (
@@ -33,8 +16,6 @@ const ReferencesBlock = ({ references }) => {
           <Reference node={ref} key={ref.parent.id} />
         ))}
       </div>
-      <hr />
-      {footer}
     </div>
   );
 };
