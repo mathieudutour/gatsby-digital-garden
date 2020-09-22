de4eb950c38481ff0f1ff2aa2b7aa705fa8ad96e
mathieudutour <mathieu@dutour.me>
2020-09-22 08:08:58 +0200

    safer index

--- packages/gatsby-theme-garden/search-indexes.js
+++ packages/gatsby-theme-garden/search-indexes.js
@@ -21,6 +21,9 @@ module.exports = (options) => {
           excerpt
           rawBody
         }
+        internal {
+          mediaType
+        }
       }
     }
   `;
--- packages/gatsby-theme-garden/should-handle-file.js
+++ packages/gatsby-theme-garden/should-handle-file.js
@@ -1,4 +1,4 @@
-module.exports = function shouldHandleFile(node, options) {
+module.exports = function shouldHandleFile(node, options = {}) {
   return (
     ((options.extensions || [".md", ".mdx"]).includes(node.ext) ||
       (options.mediaTypes || ["text/markdown", "text/x-markdown"]).includes(
