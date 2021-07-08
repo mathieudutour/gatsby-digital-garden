2a98f0be62e1fe4e04876163b76a2a3bad44a8ab
Mathieu Dutour <mathieu@dutour.me>
2021-07-08 08:56:25 +0200

    add options validation

--- packages/gatsby-source-roamresearch/gatsby-node.js
+++ packages/gatsby-source-roamresearch/gatsby-node.js
@@ -1,3 +1,22 @@
 exports.onPreBootstrap = require("./lib/on-pre-bootstrap").onPreBoostrap;
 exports.sourceNodes = require("./lib/source-nodes").sourceNodes;
 // exports.createSchemaCustomization = require("./lib/create-schema-customization").createSchemaCustomization;
+
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    url: Joi.string()
+      .required()
+      .description("Url to the Roam Research database"),
+    email: Joi.string()
+      .required()
+      .description("Email used to sign into Roam Research"),
+    password: Joi.string()
+      .required()
+      .description("Password used to sign into Roam Research"),
+    headless: Joi.boolean()
+      .default(false)
+      .description(
+        "Whether to use Puppeteer in headless mode to fetch the Roam Research data or not"
+      ),
+  });
+};
