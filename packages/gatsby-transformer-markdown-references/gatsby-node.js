e8a385725f9e1a0e2be40c3c5a2fb0fd71aa6a3a
Mathieu Dutour <mathieu@dutour.me>
2021-07-08 09:00:05 +0200

    add options validation

--- packages/gatsby-transformer-markdown-references/gatsby-node.js
+++ packages/gatsby-transformer-markdown-references/gatsby-node.js
@@ -1,3 +1,12 @@
 exports.onCreateNode = require("./lib/on-create-node.js").onCreateNode;
 exports.createSchemaCustomization = require("./lib/create-schema-customization").createSchemaCustomization;
 exports.setFieldsOnGraphQLNodeType = require("./lib/create-schema-customization").setFieldsOnGraphQLNodeType;
+
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    types: Joi.array()
+      .items(Join.string())
+      .default(["Mdx"])
+      .description("The types of the nodes to transform"),
+  });
+};
